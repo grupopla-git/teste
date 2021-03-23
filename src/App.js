44b57b2a05f4009838a1538e90683b766938b7bb
pla-pedro-moncores <81190803+pla-pedro-moncores@users.noreply.github.com>
2021-03-22 22:34:40 -0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -24,7 +24,7 @@ function App() {
          {/* setar variavel em ambiente teste/dev (via PowerShell)
         ($env:REACT_APP_NOT_SECRET_CODE = "abcdef") -and (npm start)
          */}
-      Variável de ambiente AWS Amplify<br>
+      Variável de ambiente AWS Amplify<br />
       REACT_APP_NOT_SECRET_CODE: <input type="text" defaultValue={process.env.REACT_APP_NOT_SECRET_CODE} />
       </form>
             </header>
