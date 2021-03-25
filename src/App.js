38b075044085aa938ca94fd36fb374c15e44988f
PEDRO J MONCORES <81190803+pla-pedro-moncores@users.noreply.github.com>
2021-03-25 17:14:21 -0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
     <div className="App">
 
         <h1>
-        <small>Voce está executando em modo <b>{process.env.NODE_ENV}</b>.</small>
+        <small>Voce está executando em modo: <b>{process.env.NODE_ENV}</b>.</small>
     </h1>
     <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
